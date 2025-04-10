e1ee8ea90be5e16aeeb06d853adca41aab25a083
Roman Zubatyuk <zubatyuk@gmail.com>
2025-04-09 23:01:00 -0400

    cli to clean cache

--- aimnet/cli.py
+++ aimnet/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from .calculators.model_registry import clear_assets
 from .train.calc_sae import calc_sae
 from .train.pt2jpt import jitcompile
 from .train.train import train
@@ -13,6 +14,7 @@ def cli():
 cli.add_command(train, name="train")
 cli.add_command(jitcompile, name="jitcompile")
 cli.add_command(calc_sae, name="calc_sae")
+cli.add_command(clear_assets, name="clear_model_cache")
 
 
 if __name__ == "__main__":
